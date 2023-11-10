d1685579c5efad6de7ffa7256f99b4e8b5b4a765
roshangarg <roshangarg28@gmail.com>
2023-11-10 13:01:52 +0530

    Cart tab onClick scroll fixed

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -68,8 +68,8 @@ const Cart = () => {
               borderBottom: isShoppingCompete
                 ? "2px solid #45B26B"
                 : activeTab === "1"
-                  ? "2px solid black"
-                  : "",
+                ? "2px solid black"
+                : "",
               cursor: "pointer",
             }}
           >
@@ -78,8 +78,8 @@ const Cart = () => {
                 background: isShoppingCompete
                   ? "#45B26B"
                   : activeTab === "1"
-                    ? "#000"
-                    : "#B1B5C3",
+                  ? "#000"
+                  : "#B1B5C3",
                 marginRight: "1rem",
               }}
             >
@@ -103,8 +103,8 @@ const Cart = () => {
                 color: isShoppingCompete
                   ? "#45B26B"
                   : activeTab === "1"
-                    ? "black"
-                    : "#B1B5C3",
+                  ? "black"
+                  : "#B1B5C3",
               }}
             >
               Shopping cart
@@ -126,8 +126,8 @@ const Cart = () => {
               borderBottom: isCheckoutComplete
                 ? "2px solid #45B26B"
                 : activeTab === "2"
-                  ? "2px solid black"
-                  : "",
+                ? "2px solid black"
+                : "",
             }}
           >
             <IconButton
@@ -135,8 +135,8 @@ const Cart = () => {
                 background: isCheckoutComplete
                   ? "#45B26B"
                   : activeTab === "2"
-                    ? "#000"
-                    : "#B1B5C3",
+                  ? "#000"
+                  : "#B1B5C3",
                 marginRight: "1rem",
               }}
             >
@@ -147,8 +147,8 @@ const Cart = () => {
                     isCheckoutComplete
                       ? "#45B26B"
                       : activeTab === "2"
-                        ? "white"
-                        : "#FCFCFD"
+                      ? "white"
+                      : "#FCFCFD"
                   }
                   width={30}
                   height={30}
@@ -166,8 +166,8 @@ const Cart = () => {
                 color: isCheckoutComplete
                   ? "#45B26B"
                   : activeTab === "2"
-                    ? "black"
-                    : "#B1B5C3",
+                  ? "black"
+                  : "#B1B5C3",
               }}
             >
               Checkout details{" "}
@@ -188,8 +188,8 @@ const Cart = () => {
               borderBottom: cartComplete
                 ? "2px solid #45B26B"
                 : activeTab === "3"
-                  ? "2px solid black"
-                  : "",
+                ? "2px solid black"
+                : "",
             }}
           >
             <IconButton
@@ -197,8 +197,8 @@ const Cart = () => {
                 background: cartComplete
                   ? "#45B26B"
                   : activeTab === "3"
-                    ? "#000"
-                    : "#B1B5C3",
+                  ? "#000"
+                  : "#B1B5C3",
                 marginRight: "1rem",
               }}
             >
@@ -209,8 +209,8 @@ const Cart = () => {
                     cartComplete
                       ? "#45B26B"
                       : activeTab === "3"
-                        ? "white"
-                        : "#FCFCFD"
+                      ? "white"
+                      : "#FCFCFD"
                   }
                   width={30}
                   height={30}
@@ -228,8 +228,8 @@ const Cart = () => {
                 color: cartComplete
                   ? "#45B26B"
                   : activeTab === "3"
-                    ? "black"
-                    : "#B1B5C3",
+                  ? "black"
+                  : "#B1B5C3",
               }}
             >
               Order Complete{" "}
