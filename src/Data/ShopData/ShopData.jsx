933cf72fb6d3fbe4531a507837e99a9e553bf8aa
parwez-zafar <parwezzafar3@gmail.com>
2023-11-13 21:22:05 +0530

    shop Page

---
+++ src/Data/ShopData/ShopData.jsx
@@ -0,0 +1,123 @@
+import bamboo_basket from '../../assets/images/Shop Page/bamboo basket.png';
+import black_brow_side_table from '../../assets/images/Shop Page/black brow side table.png'
+import black_tray_table from '../../assets/images/Shop Page/black tray table.png'
+import cozy_sofa from '../../assets/images/Shop Page/cozy sofa.png'
+import light_beige_pillow from '../../assets/images/Shop Page/light beige pillow.png'
+import loveseat_sofa from '../../assets/images/Shop Page/Loveseat Sofa.png'
+import luxury_sofa from '../../assets/images/Shop Page/Luxury Sofa.png'
+import off_white_pillow from '../../assets/images/Shop Page/off white pillow.png'
+import table_lamp1 from '../../assets/images/Shop Page/table lamp1.png'
+import table_lamp2 from '../../assets/images/Shop Page/table lamp2.png'
+import table_lamp3 from '../../assets/images/Shop Page/table lamp3.png'
+import white_drawer_unit from '../../assets/images/Shop Page/white drawer unit.png'
+
+const ShopData = [
+    {
+        src: loveseat_sofa,
+        alt: "Loveseat Sofa",
+        name: `Loveseat Sofa`,
+        discountPrice: 199.99,
+        price: 400.00,
+        category: 'Living Room'
+    },
+    {
+        src: luxury_sofa,
+        alt: "Luxury Sofa",
+        name: `Luxury Sofa`,
+        discountPrice: 299.99,
+        price: 500.00,
+        category: 'Living Room'
+    },
+    {
+        src: table_lamp1,
+        alt: "table_lamp1",
+        name: `Table Lamp`,
+        discountPrice: 19.99,
+        // price: 500.00,
+        category: 'Bedroom'
+    },
+    {
+        src: cozy_sofa,
+        alt: "cozy_sofa",
+        name: `Cozy Sofa`,
+        discountPrice: 299.00,
+        // price: 500.00,
+        category: 'Living Room'
+    },
+    {
+        src: white_drawer_unit,
+        alt: "white_drawer_unit",
+        name: `White Drawer unit`,
+        discountPrice: 89.99,
+        // price: 500.00,
+        category: 'Bedroom'
+    },
+    {
+        src: black_tray_table,
+        alt: "Black Tray table",
+        name: `Black Tray table`,
+        discountPrice: 89.99,
+        // price: 500.00,
+        category: 'Dinning'
+    },
+    {
+        src: table_lamp2,
+        alt: "Table  Lamp",
+        name: `Table  Lamp`,
+        discountPrice: 19.00,
+        // price: 500.00,
+        category: 'Bedroom'
+    },
+    {
+        src: black_brow_side_table,
+        alt: "Black Brow Side table",
+        name: `Black Brow Side table`,
+        discountPrice: 16.99,
+        // price: 500.00,
+        category: 'Dinning'
+    },
+    {
+        src: light_beige_pillow,
+        alt: "Light Beige Pillow",
+        name: `Light Beige Pillow`,
+        discountPrice: 3.99,
+        // price: 500.00,
+        category: 'Bedroom'
+    },
+    {
+        src: table_lamp3,
+        alt: "Table Lamp",
+        name: `Table Lamp`,
+        discountPrice: 39.99,
+        // price: 500.00,
+        category: 'Dinning'
+    },
+    {
+        src: bamboo_basket,
+        alt: "Bamboo Basket",
+        name: `Bamboo Basket`,
+        discountPrice: 9.99,
+        // price: 500.00,
+        category: 'Outdoor'
+    },
+    {
+        src: off_white_pillow,
+        alt: "off-white Pillow",
+        name: `off-white Pillow`,
+        discountPrice: 7.99,
+        // price: 500.00,
+        category: 'Bedroom'
+    },
+    {
+        src: off_white_pillow,
+        alt: "off-white Pillow",
+        name: `off-white Pillow`,
+        discountPrice: 7.99,
+        // price: 500.00,
+        category: 'Bedroom'
+    },
+
+
+];
+
+export default ShopData;
